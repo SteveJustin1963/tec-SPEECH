71c66156b44da09372efd523e2b821438718b1e2
SteveJustin1963 <stevejustin1963@gmail.com>
2020-02-06 11:35:50 +1100

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,11 +1,11 @@
-.org  0900h
-      LD HL,0910
-      LD A,(HL)
-      CP FF
-      JR Z,090D
-      OUT (05),A
-      INC HL
-      JR,0903
-      HALT
-      JR,0900
+.org  0900h       
+      LD HL,0910  ;HL = Points to start of table
+      LD A,(HL)   ;Get next Allophone
+      CP FF       ;End of table ?
+      JR Z,090D   ;Yes, HALT
+      OUT (05),A  ;Speak allophone
+      INC HL      ;Next allophone
+      JR,0903     ;Say next...
+      HALT        ;EOT, stop until key pressed
+      JR,0900     ;Key pressed, say again
 
