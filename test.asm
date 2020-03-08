d2b3ce276b761af6fc61b0fa0c8db0aee6a349f3
Steve Justin <stevejustin1963@gmail.com>
2020-03-08 14:37:29 +1100

    Update test.asm

--- test.asm
+++ test.asm
@@ -11,17 +11,17 @@
                   ;Your allophones are entered from 0910 onwards
                   ;
                   ;this says 'TALKING COMPUTER'
-                  ;0910 OD 17 17 02 2A OC 2C 04
-                  ;0918 04 2A OF 10 00 31 16 OD
+                  ;0910 0D 17 17 02 2A 0C 2C 04
+                  ;0918 04 2A 0F 10 00 31 16 0D
                   ;0920 33 04 04 FF
                   ;
                   ;Here is another greeting message. The TEC introduces itself here!
                   ;0910 1B 07 2D 35 00 36 07 2F
                   ;0918 04 06 00 1A 10 00 12 13
-                  ;0920 00 OD 13 03 13 03 37 13
+                  ;0920 00 0D 13 03 13 03 37 13
                   ;0928 03 08 18 10 09 31 16 11
                   ;0930 33 04 04 04 38 20 00 30
-                  ;0938 OC 1D37 09 13 32 04 FF
+                  ;0938 0C 1D37 09 13 32 04 FF
                   ;
                   ;
                   
